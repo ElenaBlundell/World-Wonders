41854129836b6b0f3be2d550ca27deea68be6382
Elena Blundell <blundell.elena@gmail.com>
2023-10-13 16:12:55 -0500

    Update Header.jsx

--- components/Header.jsx
+++ components/Header.jsx
@@ -3,6 +3,6 @@ import React from "react"
 export default function Header(){
     return <header>
             <h2>Wonders of the World</h2>
-            <img src="/images/globe-icon.png" />
+            <img src="./images/globe-icon.png" />
           </header>
 }
